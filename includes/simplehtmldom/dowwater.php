2cabf9d3a6312f0e7fba1e4b5da5b22de75ddf93
Thomas Hunter <tlhunter@gmail.com>
2012-04-29 13:23:30 -0400

    Fixing simple html dom location

--- includes/simplehtmldom/dowwater.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('simple_html_dom.php');
-include_once('spider.php');
-
-$website_url = 'http://preview-www.dowwaterandprocess.com/';
-$file = 'test.htm';
-
-$spider = New Spider($website_url, $file);
-$spider->crawl();
